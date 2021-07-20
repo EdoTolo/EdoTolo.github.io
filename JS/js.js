69bb24a2dd30f2dfd1ff79fc29ea65b49e43a7cb
Edo Tolo <69632089+EdoTolo@users.noreply.github.com>
2021-07-19 20:39:50 -0700

    .

--- JS/js.js
+++ JS/js.js
@@ -1,9 +1,11 @@
+/*
 //Dark Mode
 function darkMode() {
 	document.body.classList.toggle("dM");
 	//checkCookie();
 }
-/*/
+
+//Theme
 function checkCookie(){
 	var themeLM = document.body.class = "dM";
 	var themeDM = document.body.class = "dM";
@@ -16,27 +18,11 @@ function checkCookie(){
 	}
 	document.cookie = theme;
 }
-//*/
-
-
+//
 
 //Scroll Back To Top
 function gotoTop() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 }
-
-
-
-//Hidden Codes
-function codeNumber() {
-	let n = +prompt('Enter a code...','');
-	switch (n) {
-		case 202420:
-			alert("Code Accepted\n\nRedirecting...");
-			window.location = "sites/template.html";
-			break;
-		default:
-		break;
-	}
-}
+*/
