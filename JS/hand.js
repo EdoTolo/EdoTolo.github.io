2b45780c562a9560a40dbf36bed17ed93a6b3135
Edo Tolo <eduardotolentino03@gmail.com>
2025-04-01 14:27:42 -0800

    .

---
+++ JS/hand.js
@@ -0,0 +1,18 @@
+var doc = document.getElementById;
+var a = doc("t");
+var b = doc("f");
+var c = doc("m");
+var d = doc("i");
+var e = doc("p");
+var f = doc("a");
+
+if(f.checked = true){
+    b.checked = true;
+}
+else{
+    b.checked = false;
+}
+
+function letToNum(){
+
+};
